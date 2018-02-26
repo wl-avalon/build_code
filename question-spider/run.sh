36e0deccba141fd72e5a28d4442f66faf52acf4b
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-26 21:00:50 +0800

    提交代码

--- question-spider/run.sh
+++ question-spider/run.sh
@@ -3,6 +3,8 @@ MODULE_NAME="question-spider"
 SOURCE_CODE_PATH="/home/saber/source_code/${MODULE_NAME}";
 APPLICATION_PATH="/home/saber/webroot/study-palace/"
 cd ${SOURCE_CODE_PATH};
+git checkout .;
+git pull;
 sh build.sh;
 cd output;
 tar -zxvf ${MODULE_NAME}.tar.gz

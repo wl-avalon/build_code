be58df72245c29bfbae45f12367fb4afeb0da251
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-26 20:43:14 +0800

    提交代码

---
+++ question-ui/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+MODULE_NAME="question-ui"
+SOURCE_CODE_PATH="/home/saber/source_code/${MODULE_NAME}";
+APPLICATION_PATH="/home/saber/webroot/study-palace/"
+cd ${SOURCE_CODE_PATH};
+sh build.sh;
+cd output;
+tar -zxvf ${MODULE_NAME}.tar.gz
+cp -r application/* ${APPLICATION_PATH}
+cd ${SOURCE_CODE_PATH};
+rm -r output

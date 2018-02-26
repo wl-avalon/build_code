f4c173ed54925b31c9517c3041e1ffb01a0a5f13
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-02-26 12:17:53 +0800

    提交代码

--- question-spider/run.sh
+++ question-spider/run.sh
@@ -4,5 +4,6 @@ SOURCE_CODE_PATH="/home/saber/source_code/${MODULE_NAME}";
 APPLICATION_PATH="/home/saber/webroot/study-palace/"
 cd ${SOURCE_CODE_PATH};
 sh build.sh;
+tar -zxvf output/${MODULE_NAME}.tar.gz output/
 cp -r output/application/* ${APPLICATION_PATH}
 rm -r output

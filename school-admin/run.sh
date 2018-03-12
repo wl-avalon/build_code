0c973924193659936dc1723d1a26ee21c0285752
萨耶摩尔丶槿花 <wzj-dev@AvalonSpacedeMacBook-Pro.local>
2018-03-12 18:40:03 +0800

    提交代码

--- school-admin/run.sh
+++ school-admin/run.sh
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
+MODULE_NAME="school-admin"
+SOURCE_CODE_PATH="/home/saber/source_code/${MODULE_NAME}";
+APPLICATION_PATH="/home/saber/webroot/study-palace/"
+cd ${SOURCE_CODE_PATH};
+git checkout .;
+git pull;
+sh build.sh;
+cd output;
+tar -zxvf ${MODULE_NAME}.tar.gz
+cp -r application/* ${APPLICATION_PATH}
+cd ${SOURCE_CODE_PATH};
+rm -r output
